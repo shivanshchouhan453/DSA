4bac3c245b001d06e286e997e1bb4be0bc491b6d
shivanshchouhan453 <cshivansh453@gmail.com>
2026-09-03 16:30:55 +0530

    Basics : All Basic Question of Patterns

---
+++ CPP/basics/baiscs_programs.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int amanAge = 25;
+    int syamAge = 22;
+    int ramanAge = 35;
+
+    cout << "Aman Age is : " << amanAge;
+
+    return 0;
+}
---
+++ CPP/basics/star_pattern_5.cpp
@@ -0,0 +1,22 @@
+// *****
+// ****
+// ***
+// **
+// *
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // print start
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 6 - i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
