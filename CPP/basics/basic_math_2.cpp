a8f15fd084d1ab3a34ee2cb0ff0288da08978f7d
shivanshchouhan453 <cshivansh453@gmail.com>
2026-09-04 13:05:54 +0530

    Basics : reverse the number(optimal approch)

--- CPP/basics/basic_math_2.cpp
+++ CPP/basics/basic_math_2.cpp
@@ -1,33 +1,23 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+// brute force approach
+//
+
+// optimize approach
+
 int main()
 {
-
-    int number = 7789;
     int reverseNum = 0;
-    vector<int> arr;
+    int number = 12345;
 
-    // take out all the digits and put in vector
     while (number != 0)
     {
-        int rem = number % 10;
+        int reminder = number % 10;
+        reverseNum = reverseNum * 10 + reminder;
         number = number / 10;
-        arr.push_back(rem);
     }
-
-    // pop out all items and calculate
-
-    int iter = 1;
-    while (arr.size() != 0)
-    {
-        int digit = arr[(arr.size() - 1)];
-        arr.pop_back();
-        reverseNum = reverseNum + digit * iter;
-        iter = iter * 10;
-    }
-
-    cout << " Reverse Number is " << reverseNum;
-
+    cout << reverseNum;
     return 0;
 }
