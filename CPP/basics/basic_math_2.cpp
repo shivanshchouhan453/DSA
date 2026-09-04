aa09fa07904bab8f3d071239c9ae1303cd556b8f
shivanshchouhan453 <cshivansh453@gmail.com>
2026-09-04 14:46:28 +0530

    Basics : reverse the number(with constraints)

--- CPP/basics/basic_math_2.cpp
+++ CPP/basics/basic_math_2.cpp
@@ -10,14 +10,25 @@ using namespace std;
 int main()
 {
     int reverseNum = 0;
-    int number = 12345;
+    int number = -123;
 
     while (number != 0)
     {
         int reminder = number % 10;
+        // cout << "RM : " << reminder << endl;
         reverseNum = reverseNum * 10 + reminder;
+        // cout << "RS : " << reverseNum << endl;
         number = number / 10;
     }
     cout << reverseNum;
+
+    if (reverseNum < INT32_MIN || reverseNum > INT32_MAX)
+    {
+        return 0;
+    }
+    else
+    {
+        return reverseNum;
+    }
     return 0;
 }
