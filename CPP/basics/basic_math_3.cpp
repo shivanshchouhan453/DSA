2cd630d501ff7e0ea44d55b453199e2acf854198
shivanshchouhan453 <cshivansh453@gmail.com>
2026-09-07 10:23:12 +0530

    Que: greatest common divisor

---
+++ CPP/basics/basic_math_3.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n1 = 5;
+    int n2 = 20;
+    int minimum = 0;
+    int gcf = 1;
+    int count = 0;
+
+    // set minimum
+    if (n1 < n2)
+    {
+        minimum = n1;
+    }
+    else
+    {
+        minimum = n2;
+    }
+
+    // iterate all minimum divisor  ==> iteration are too much
+    // for (int i = 1; i <= minimum; i++)
+    // {
+    //     if (n1 % i == 0 && n2 % i == 0)
+    //     {
+    //         gcf = i;
+    //     }
+    //     count++;
+    // }
+
+    // iterate from high to low it reduce the iteration
+    for (int i = minimum; i >= 1; i--)
+    {
+        if (n1 % i == 0 && n2 % i == 0)
+        {
+            gcf = i;
+            break;
+        }
+        count++;
+    }
+
+    cout << "Greatest Common Factor is : " << gcf << endl;
+    cout << count;
+    return 0;
+}
